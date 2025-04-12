81afbfcf39f6164e67a00687497991f5f4db0e39
Adorján Pál <adorjan3270@gmail.com>
2025-04-12 19:18:24 +0200

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () =>
+            import('./pages/auth/auth.component').then((m) => m.AuthComponent),
+    },
+];
